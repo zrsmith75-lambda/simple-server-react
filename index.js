fa239bfac6ae06c320134456f73a6c838405f795
Zac Smith <zrs3141592@gmail.com>
2020-03-09 17:00:38 -0600

    add array

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-server.get("/api/books");
+let books = [
+  {
+    title: "Vader rocks",
+    year: 1975,
+    id: 1
+  },
+  {
+    title: "Vader is awesome",
+    year: 1977,
+    id: 2
+  },
+  {
+    title: "Vader is so cool",
+    year: 1981,
+    id: 3
+  }
+];
+
+server.get("/api/books", (req, res) => {
+  res.status(200).json(books);
+});
 
 const PORT = 5500;
 server.listen(PORT, () =>
